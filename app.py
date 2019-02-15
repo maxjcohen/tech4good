4dce545681ac72b9de5c2d6a38a59360d9d4f697
max cohen <max.cohen@gmx.com>
2019-02-16 00:09:05 +0100

    Added drawing function

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
+import io
+import base64
+
 from flask import Flask
 from flask import render_template
 import numpy as np
+import matplotlib.pyplot as plt
 
 from src.getdata import get_numbers
 
 app = Flask(__name__)
+
+@app.route('/plot')
+def build_plot():
+
+	img = io.BytesIO()
+
+	y = [1,2,3,4,5]
+	x = [0,2,1,3,4]
+	plt.plot(x,y)
+	plt.savefig(img, format='png')
+	img.seek(0)
+
+	plot_url = base64.b64encode(img.getvalue()).decode()
+
+	# return '<img src="data:image/png;base64,{}">'.format(plot_url)
+	return render_template('index.html', plot=plot_url)
 
 @app.route('/')
 def hello_world():
