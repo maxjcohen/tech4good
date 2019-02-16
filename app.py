e50bae9bbcd729e38b4a17e8f5e87ed340407b30
max cohen <max.cohen@gmx.com>
2019-02-16 01:36:47 +0100

    Removed old plot

--- app.py
+++ app.py
@@ -1,36 +1,13 @@
-import io
-import base64
-
 from flask import Flask
 from flask import render_template
 from flask import Response
 from flask import jsonify
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 
 from src.getdata import get_numbers
 from src.crunch_numbers import text_data
 
 app = Flask(__name__)
-
-
-@app.route('/images/plot.png')
-def plot_png():
-    # Create figure
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = np.random.randint(1, 50, size=len(xs))
-    axis.plot(xs, ys)
-
-    # Convert figure to bytes
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-
-    # Returns image
-    return Response(output.getvalue(), mimetype='image/png')
 
 @app.route('/data/json')
 def get_json():
