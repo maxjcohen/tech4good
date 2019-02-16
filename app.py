a0d98f24a5cbdf6aef37cff1895b29c782506f47
max cohen <max.cohen@gmx.com>
2019-02-16 01:19:51 +0100

    changed tab to spaces

--- app.py
+++ app.py
@@ -16,45 +16,45 @@ app = Flask(__name__)
 
 @app.route('/images/plot.png')
 def plot_png():
-	# Create figure
-	fig = Figure()
-	axis = fig.add_subplot(1, 1, 1)
-	xs = range(100)
-	ys = np.random.randint(1, 50, size=len(xs))
-	axis.plot(xs, ys)
+    # Create figure
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = np.random.randint(1, 50, size=len(xs))
+    axis.plot(xs, ys)
 
-	# Convert figure to bytes
-	output = io.BytesIO()
-	FigureCanvas(fig).print_png(output)
+    # Convert figure to bytes
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
 
-	# Returns image
-	return Response(output.getvalue(), mimetype='image/png')
+    # Returns image
+    return Response(output.getvalue(), mimetype='image/png')
 
 @app.route('/data/json')
 def get_json():
-	# Get data
-	data = get_numbers()
+    # Get data
+    data = get_numbers()
 
-	return jsonify(data)
-	
+    return jsonify(data)
+
 @app.route('/')
 def hello_world():
-	# Get data
-	data = get_numbers()
-	
-	# Crunch numbers
-	n_prostitues = np.sum([[prostitue for prostitue in data["prostitues"][region].values()] \
-		for region in data["prostitues"]])
+    # Get data
+    data = get_numbers()
 
-	n_benevoles = np.sum([region for region in data["benevoles"].values()])
+    # Crunch numbers
+    n_prostitues = np.sum([[prostitue for prostitue in data["prostitues"][region].values()] \
+        for region in data["prostitues"]])
 
-	n_sensibilises = np.sum([region for region in data["sensibilises"].values()])
+    n_benevoles = np.sum([region for region in data["benevoles"].values()])
 
-	# Return template filled with data
-	feed = {
-		"n_prostitues": n_prostitues,
-		"n_benevoles": n_benevoles,
-		"n_sensibilises": n_sensibilises
-	}
+    n_sensibilises = np.sum([region for region in data["sensibilises"].values()])
 
-	return render_template('index.html', **feed)
+    # Return template filled with data
+    feed = {
+        "n_prostitues": n_prostitues,
+        "n_benevoles": n_benevoles,
+        "n_sensibilises": n_sensibilises
+    }
+
+    return render_template('index.html', **feed)
