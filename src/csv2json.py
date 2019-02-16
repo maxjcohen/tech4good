cad7c370d3b15ac2d281083d73f3c2c31257ad61
k3lyan <kelyan.morfouesse@gmail.com>
2019-02-16 04:37:42 +0100

    upload preprocess modules

---
+++ src/csv2json.py
@@ -0,0 +1,26 @@
+import json
+import pandas as pd
+
+def filter_data():
+    df = pd.read_csv('data.csv', sep='\t')
+    df_filled = df.fillna(0, inplace=True)
+    df_local = df.groupby(['Votre délégation départementale']).sum()
+    data = {'actions': {'visites': {}, 'permanences': {}, 'formation': {}, 'prevention': {}, 'participants': {}}}
+    for index, row in df_local.iterrows():
+        data['actions']['visites'][index] = {'total': row[0], 'femmes': row[1], 'hommes': row[2], 'trans': row[3]}        
+        data['actions']['permanences'][index] = {'total': row[5], 'femmes': row[6], 'hommes': row[7], 'trans': row[8]}
+        data['actions']['formation'][index] = {'total': row[9]}
+        data['actions']['participants'][index] = {'total': row[11]} 
+        data['actions']['prevention'][index] = {'total': row[10]}
+    data['benevoles'] = df['test'].nunique() 
+    return data
+
+
+def stats():    
+    dic = filter_data()
+    with open('data.json', 'w') as outfile:
+        json.dump(dic, outfile, sort_keys=True, indent=4)
+
+if __name__ == '__main__':
+    stats()
+
---
+++ src/gsheet2csv.py
@@ -0,0 +1,25 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import csv
+
+
+def get_sheet():
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    # Make sure you use the right name here !
+    return client.open("Questionnaire terrain Mouvement du Nid (Responses)").sheet1
+
+def save_csv(sheet):
+    # Extract and print all of the values
+    with open('data.csv', 'w', newline='') as f:
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerows(sheet.get_all_values())
+
+def main():
+    sheet = get_sheet()
+    save_csv(sheet)
+
+if __name__ == '__main__':
+    main()
